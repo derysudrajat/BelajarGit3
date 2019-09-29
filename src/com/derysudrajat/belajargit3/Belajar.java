8b65aeb23d5c4993523499c2b88320b5813d8e1f
derysudrajat <dery.sudrajat17@gmail.com>
2019-09-29 11:19:29 +0700

    Menambahkan Objek Manusia ke dalam kelas

--- src/com/derysudrajat/belajargit3/Belajar.java
+++ src/com/derysudrajat/belajargit3/Belajar.java
@@ -1,18 +1,21 @@
 package com.derysudrajat.belajargit3;
-
+import static com.derysudrajat.belajargit3.Manusia.*;
 public class Belajar {
     public static void main(String[] args) {
         int umur;
         String nama;
-        String cita;
+        String jabatan;
 
         System.out.println("Hello Github, Saya udah ubah tulsannya");
 
         umur = 17;
         nama = "Si Bejo";
-        cita = "Penyihir";
-        System.out.println("Nama: "+nama+"\n" +
-                "Umur: "+umur+"\n" +
-                "Cita-Cita: "+cita);
+        jabatan = "Penyihir";
+
+        Manusia bejo = new Manusia(umur,nama,jabatan);
+
+        System.out.println("Nama: "+bejo.getNama()+"\n" +
+                "Umur: "+bejo.getUmur()+"\n" +
+                "Cita-Cita: "+bejo.getJabatan());
     }
 }
