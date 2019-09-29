3edda7677fffb0ec108e51aacdf4e770fb9ef7cb
derysudrajat <dery.sudrajat17@gmail.com>
2019-09-29 10:44:02 +0700

    Mengubah tulisan Hello World

--- src/com/derysudrajat/belajargit3/Belajar.java
+++ src/com/derysudrajat/belajargit3/Belajar.java
@@ -2,6 +2,6 @@ package com.derysudrajat.belajargit3;
 
 public class Belajar {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        System.out.println("Hello Github, Saya udah ubah tulsannya");
     }
 }
