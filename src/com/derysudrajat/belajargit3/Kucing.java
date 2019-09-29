42f762a8cd514bbb4d2685208f2ce2193087f6f8
derysudrajat <dery.sudrajat17@gmail.com>
2019-09-29 10:56:41 +0700

    Menambahkan File baru Kucing.java berisi method bicara

---
+++ src/com/derysudrajat/belajargit3/Kucing.java
@@ -0,0 +1,14 @@
+package com.derysudrajat.belajargit3;
+
+
+public class Kucing {
+
+    private static void bicara(){
+        System.out.println("Meowww");
+    }
+
+    public static void main(String[] args) {
+        bicara();
+    }
+
+}
