11caca9a252ae34273691bc8e66ee069a5e7c5d9
derysudrajat <dery.sudrajat17@gmail.com>
2019-09-29 10:56:09 +0700

    Menambahkan varibel untuk menampilkan informasi Si Bejo

--- src/com/derysudrajat/belajargit3/Belajar.java
+++ src/com/derysudrajat/belajargit3/Belajar.java
@@ -2,6 +2,17 @@ package com.derysudrajat.belajargit3;
 
 public class Belajar {
     public static void main(String[] args) {
+        int umur;
+        String nama;
+        String cita;
+
         System.out.println("Hello Github, Saya udah ubah tulsannya");
+
+        umur = 17;
+        nama = "Si Bejo";
+        cita = "Penyihir";
+        System.out.println("Nama: "+nama+"\n" +
+                "Umur: "+umur+"\n" +
+                "Cita-Cita: "+cita);
     }
 }
